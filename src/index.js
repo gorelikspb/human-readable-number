13623999b7fe9a3c8df6477fe3abc9c77a8001b3
Your <you@example.com>
2021-03-15 13:39:57 +0300

    feat: human readable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+    if (num == 0){
+        return 'zero'
+    }
+    let units = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+    let tens = [ '', '',  'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+
+    num = num.toString()
+    num = num.split('').reverse()
+
+    let order = 0
+    let out = ''
+    for (let dig  of num){
+        if (order == 1 && dig == 1){ // .. teen case
+            out = units[parseInt(num[1]+num[0])]
+        }
+        else if (order == 0){
+            out = units[parseInt(dig)]
+        }
+        
+        else if (order == 1 && dig !=0){
+            out = tens[dig] + ' ' + out
+        }
+
+        else if (order == 2){
+            // hundreds = 'hundred'
+            // if (dig > 1){
+            //     hundreds +='s'
+            // }
+            out = units[dig] + '' +'hundred' + ' ' + out
+        }
+
+        order+=1
+
+    }
+    return out.trimRight()             
 }
